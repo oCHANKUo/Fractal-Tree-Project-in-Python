baa49238e7655513aeaae22714153c12b59657e7
Chanuka Wijesinghe <chanuka.s.wijesinghe@gmail.com>
2025-06-23 18:32:33 +0530

    updates

--- fractal-tree.py
+++ fractal-tree.py
@@ -1,3 +1,19 @@
 from turtle import *
-from theme import set_theme
+#from theme import set_theme
 import random
+
+#function to grow tree, recursive
+def grow(length, decrease, angle, noise=0):
+
+    if length > 10:
+        forward(length)
+
+
+penup()
+goto(0, -400)
+pendown()
+left(90)
+grow(150, 1, 0)
+
+tracer(0)
+exitonclick()
