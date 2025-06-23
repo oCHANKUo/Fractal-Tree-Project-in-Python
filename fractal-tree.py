f0f4866921a965af304c943022ad9e474a69fd29
Chanuka Wijesinghe <chanuka.s.wijesinghe@gmail.com>
2025-06-23 18:36:08 +0530

    changes

--- fractal-tree.py
+++ fractal-tree.py
@@ -7,13 +7,24 @@ def grow(length, decrease, angle, noise=0):
 
     if length > 10:
         forward(length)
+        new_length = length * decrease
+        angle_l = angle
+        angle_r = angle
+
+        left(angle_l)
+        grow(new_length, decrease, angle, noise)
+        right(angle_l)
+
+        right(angle_r)
+        grow(new_length, decrease, angle, noise)
+        right(angle_r)
 
 
 penup()
 goto(0, -400)
 pendown()
 left(90)
-grow(150, 1, 0)
+grow(150, 0.8, 0)
 
 tracer(0)
 exitonclick()
